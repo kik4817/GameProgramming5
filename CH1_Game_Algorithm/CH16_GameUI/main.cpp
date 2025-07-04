b41148529f85758d0783bad3150547d59dbf502e
kik4817 <kik4817@naver.com>
2025-07-04 17:51:01 +0900

    (2025-07-04)게임 데이터를 컨테이너에 출력하기

--- CH1_Game_Algorithm/CH16_GameUI/main.cpp
+++ CH1_Game_Algorithm/CH16_GameUI/main.cpp
@@ -5,6 +5,9 @@
 
 #include <iostream>
 #include <Windows.h>
+#include <queue>
+#include <string>
+#include "InventoryDP.h"
 
 using namespace std;
 
@@ -28,7 +31,7 @@ public:
 
 	static void Print(const string& BG, const string& textColor, const string& message)
 	{
-		cout << BG << textColor << message << Color::RESET << endl;
+		cout << BG << textColor << message << Color::RESET;
 	}
 };
 
@@ -88,6 +91,7 @@ void Printmenu()
 	cout << Color::BG_BLUE << Color::RED << "2." << Color::RESET << Color::BG_RED << "불러오기" << Color::RESET << endl;
 	//cout << "3." << "설정" << endl;
 	Color::Print(Color::BG_GREEN, Color::BLACK, "3.설정");
+	cout << endl;
 	cout << "4." << "종료" << endl;
 }
 
@@ -111,45 +115,224 @@ void PrintProgressBar(int current, int total, int width = 30)
 
 void LoadingAnimation()
 {
+	const int total = 20;
+	const int width = 20;
+
 	cout << "로딩 진행 현황을 그려준다." << endl;
-	for (int i = 0;i < 20;i++)
+	for (int i = 0;i < total;i++) // i<=total -> 100%
 	{
-		PrintProgressBar(i, 20);
+		system("cls");
+		PrintProgressBar(i, total, width);
 		cout << endl;		
 		cout.flush();
-		Sleep(100);
-		system("cls");
+		Sleep(50);		
 	}
 
 	// vector자료구조에 진행현황을 그림으로 저장을 했다가 갱신을 하는 방식. 
 }
 
-void DrawBox()
+/*
+* gotoXY(int x, int y)
+*/
+
+void gotoXY(int x, int y)
+{
+	COORD pos{ x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+/*
+* DrawBox를 내가 원하는 좌표에 그릴 수 이ㅆ도록 아래 함수를 수정해보세요 
+* gotoXY를 이용하세요. 탑, 몸통, 바디
+*/
+
+void DrawBox(int width, int height) // 가로, 세로
+{	
+
+	// 머리
+	cout << Color::BG_CYAN << "+";
+	//-------
+	for (int i = 0; i < width;i++)
+	{
+		cout << "-";
+	}
+	cout << "+"<<Color::RESET << endl;
+
+	// 바디
+	for (int y = 0;y < height;y++)
+	{
+		// 왼쪽 벽
+		Color::Print(Color::BG_CYAN, Color::WHITE, "|");
+
+		// 공백
+		for (int i = 0; i < width;i++)
+		{
+			cout << " ";
+		}
+
+		// 오른쪽 벽
+		Color::Print(Color::BG_CYAN, Color::WHITE, "|");
+		cout << endl;
+	}
+	
+	// 마지막	
+	cout << Color::BG_CYAN << "+";
+	//-------
+	for (int i = 0; i < width;i++)
+	{
+		cout << "-";
+	}
+	cout << "+" << Color::RESET << endl;
+}
+
+void DrawBox(int px, int py, int width, int height, const ItemW& item)
 {
 	/*
 	* 상자는 위, 중간, 아래 부분으로 구분이 됩니다.
 	* 반복문을 이요해서 표현해보세요.
 	*/
-	cout << Color::BG_CYAN<<"+----------+"<< Color::RESET << endl;
-	cout << Color::BG_CYAN<<"|"<< Color::RESET<< "          " << Color::BG_CYAN << "|" << Color::RESET<<endl;
-	Color::Print(Color::BG_CYAN, Color::WHITE, "|");
-	cout << "           ";
-	Color::Print(Color::BG_CYAN, Color::WHITE, "|");
-	cout << endl;
-	
-	cout << Color::BG_CYAN<<"+----------+"<< Color::RESET << endl;
+
+	// 머리
+	gotoXY(px, py);
+	cout << Color::BG_CYAN << "+";
+	//-------
+	for (int i = 0; i < width;i++)
+	{
+		cout << "-";
+	}
+	cout << "+" << Color::RESET << endl;
+
+	// 중간	
+	for (int y = 0;y < height;y++) 
+	{
+		gotoXY(px, py + 1 + y); // py+=1			py + 1 + y
+		// 왼쪽 벽
+		Color::Print(Color::BG_CYAN, Color::WHITE, "|");
+
+		// item의 정보를 출력
+		string content = "";
+
+		if (y == 1)
+		{
+			content = "이름 : " + item.GetName();
+		}
+		else if (y == 2)
+		{
+			content = "무게 : " + to_string(item.GetWeight());
+		}
+		else if (y == 3)
+		{
+			content = "가치 : " + to_string(item.GetValue());
+		}
+
+		if (!content.empty())
+		{
+			cout << Color::CYAN << content << Color::RESET;
+
+			for (int i = content.length();i < width;i++)
+			{
+				cout << " ";
+			}
+		}
+		else
+		{
+			for (int i = 0;i < width;i++)
+			{
+				cout << " ";
+			}
+		}
+
+		// 공백
+		for (int i = 0; i < width;i++)
+		{
+			cout << "";
+		}
+
+		// 오른쪽 벽
+		Color::Print(Color::BG_CYAN, Color::WHITE, "|");
+		cout << endl;
+	}
+
+	// 마지막
+	gotoXY(px, py + height + 1); // py+=1		py + height + 1
+	cout << Color::BG_CYAN << "+";
+	//-------
+	for (int i = 0; i < width;i++)
+	{
+		cout << "-";
+	}
+	cout << "+" << Color::RESET << endl;
+}
+
+void Test(const ItemW& item)
+{
+	cout << item.GetName() << endl;
+}
+
+void ProcedualDrawBox(int startX, int startY, vector<ItemW>& items)
+{
+	queue<ItemW> itemQueue;
+
+	//itemQueue.push(ItemW("롱소드", 80, 50));
+	//itemQueue.push(ItemW("숏소드", 20, 18));
+	//itemQueue.push(ItemW("나이프", 5, 2));
+	//itemQueue.push(ItemW("롱소드", 80, 50));
+	//itemQueue.push(ItemW("숏소드", 20, 18));
+	//itemQueue.push(ItemW("나이프", 5, 2));	
+	//itemQueue.push(ItemW("롱소드", 80, 50));
+	//itemQueue.push(ItemW("숏소드", 20, 18));
+	//itemQueue.push(ItemW("나이프", 5, 2));
+
+	for (auto& Item : items)
+	{
+		itemQueue.push(Item);
+	}
+
+	// ctrl + r + r
+	int currentRow = 0; // 현제 그려야할 도형의 x좌표
+	int currentCol = 0; // 현제 그려야할 도형의 y좌표
+
+	const int Box_Width = 20;
+	const int Box_Height = 10;
+	const int Box_RowSpacing = 5;
+	const int Box_ColSpacing = 5;
+
+	while (!itemQueue.empty())
+	{
+		ItemW currentItem = itemQueue.front();
+		itemQueue.pop();
+		Test(currentItem);
+		// ItemW 타입을 사용하는 함수를 만들어보세요.
+
+		int boxX = startX + (currentRow * (Box_RowSpacing + Box_Width));
+		int boxY = startY + (currentCol * (Box_ColSpacing + Box_Height));
+
+		// 좌표를 수정해주는 코드를 작성하세요. 3*3 박스로 아이템 그려지도록 코드를 만들어보세요.
+
+		
+		DrawBox(boxX, boxY, Box_Width, Box_Height, currentItem);
+
+		currentRow++;
+		if (currentRow >= 3)
+		{
+			currentRow = 0;
+			currentCol++;
+		}
+
+		Sleep(100);
+	}
 }
 
 int main()
 {
 	//ExampleColor();
 
-	//LoadingAnimation();
 
 	// 노란색 글자에 밑줄이 그어진 문자
 	cout << Color::BG_BLUE << Color::GREEN << "==== TITLE ====" << Color::RESET << endl;
 	
 	Color::Print(Color::BG_BLUE, Color::GREEN, "==== TITLE2 ====");
+	cout << endl;
 
 	// 로딩바, 메뉴 표시, UI표현
 
@@ -158,6 +341,38 @@ int main()
 	// 제목 Color클래스를 사용해서
 	// 1. 게임 시작
 	// 2. 게임 설정
+	//LoadingAnimation();
+	system("cls");
 
-	DrawBox();
+	//ItemW item("롱소드", 10, 10);
+	//	
+	//for (int i = 0; i < 3;i++)
+	//{
+	//	DrawBox(5+25*i, 8, 20, 10, item);
+
+	//}
+	//
+	//// 가로 세로
+	//// 어떤 좌표 작성 겹치지 않고 그릴 수 있는지
+	////DrawBox(28,8, 20,10);
+
+	InventoryM inventory(7);
+
+	// 6,4,3,5
+	ItemW A("A", 6, 13);
+	ItemW B("B", 4, 8);
+	ItemW C("C", 3, 6);
+	ItemW D("D", 5, 12);
+
+	std::vector<ItemW> selectableTable{ A,B,C,D };
+
+	std::pair<int, std::vector<ItemW>> bestItem = inventory.findBestItem(7, selectableTable);
+	//auto bestItem = inventory.findBestItem(7, selectableTable);
+
+	std::cout << "주어진 아이템의 최대 가치 : " << bestItem.first << std::endl;
+
+	std::vector<ItemW> ItemC = bestItem.second;
+
+	ProcedualDrawBox(5,5, ItemC);
+	
 }
